a375ceb14617b08791c1ad6ed278982bf9ed42c5
JoshHaokip <joshhaokip@gmail.com>
2021-04-22 20:15:59 +0100

    Added API to Mailchimp and test complete

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-
   return (
     <div className={styles.container}>
       <Head>
@@ -12,93 +11,94 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-
         <div className={styles.homeContainer}>
           <div className={styles.textContainer}>
-            <p className={styles.stackii}>
-              stackii
-            </p>
+            <p className={styles.stackii}>stackii</p>
             <h1 className={styles.title}>
               Ultimate library for building products
             </h1>
 
-
             <p className={styles.description}>
-              We aggregate the web for the best resources created by indie hackers and side project enthusiasts.
+              We aggregate the web for the best resources so you dont have to.
             </p>
 
             <div className={styles.btn}>
-
-              <a href="https://library.stackii.dev/" target="_blank"><button className={`${styles.btnCard} ${styles.btnPrimary}`}>
-                free search
-              </button></a>
+              <a href="https://library.stackii.dev/" target="_blank">
+                <button className={`${styles.btnCard} ${styles.btnPrimary}`}>
+                  my stackii
+                </button>
+              </a>
 
               <button className={`${styles.btnCard} ${styles.btnSecondary}`}>
                 get updates
               </button>
-
             </div>
-
           </div>
 
           <div className={styles.imgContainer}>
-          <lottie-player 
-            src="https://assets5.lottiefiles.com/packages/lf20_jn4vvm3d.json"  
-            background="transparent"  
-            speed="1"
-            className={styles.img}
-            loop="true"  
-            renderer='svg'
-            autoplay>
-
-          </lottie-player>
-
+            <lottie-player
+              src="https://assets5.lottiefiles.com/packages/lf20_jn4vvm3d.json"
+              background="transparent"
+              speed="1"
+              className={styles.img}
+              loop="true"
+              renderer="svg"
+              autoplay
+            ></lottie-player>
           </div>
-
         </div>
 
         <div className={styles.categoriesContainer}>
-
           <div className={styles.categoriesTitle}>
             <h2>Categories</h2>
           </div>
 
           <div className={styles.grid}>
-
             <div className={styles.card}>
               <h3>25 resources</h3>
               <h1>Web Development</h1>
-              <p>Learn the basics of web development with some of the best resources on the web. From learning platforms to libraries, you'll have everything you need to start building.</p>
+              <p>
+                Learn the basics of web development with some of the best
+                resources on the web. From learning platforms to libraries,
+                you'll have everything you need to start building.
+              </p>
               <h4>Start coding</h4>
             </div>
 
             <div className={styles.card}>
               <h3>33 resources</h3>
               <h1>Design</h1>
-              <p>Need help understanding UX/UI? Struggling to wireframe your product? All the resources and tools you'll need to create your next masterpiece.</p>
+              <p>
+                Need help understanding UX/UI? Struggling to wireframe your
+                product? All the resources and tools you'll need to create your
+                next masterpiece.
+              </p>
               <h4>Start designing</h4>
             </div>
 
             <div className={styles.card}>
               <h3>42 resources</h3>
               <h1>Tools and Utils</h1>
-              <p>No superhero is without their trusted sidekick! Power up your products with the best tools on the web. From web analytics to automation, you'll never have to struggle again.</p>
+              <p>
+                No superhero is without their trusted sidekick! Power up your
+                products with the best tools on the web. From web analytics to
+                automation, you'll never have to struggle again.
+              </p>
               <h4>Start making</h4>
             </div>
 
             <div className={styles.card}>
               <h3>17 resources</h3>
               <h1>Startups</h1>
-              <p>You've built your product and added some additional tools, now's the time to start spreading your product all over the web.</p>
+              <p>
+                You've built your product and added some additional tools, now's
+                the time to start spreading your product all over the web.
+              </p>
               <h4>Start shouting</h4>
             </div>
-
           </div>
-
         </div>
-
       </main>
-
     </div>
-  )
+  );
 }
---
+++ pages/newsletter.js
@@ -0,0 +1,56 @@
+import React, { useRef, useState } from "react";
+import styles from "../styles/Newsletter.module.css";
+
+const Newsletter = () => {
+  const inputEl = useRef(null);
+  const [message, setMessage] = useState("");
+
+  const subscribe = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch("/api/subscribe", {
+      body: JSON.stringify({
+        email: inputEl.current.value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+
+    const { error } = await res.json();
+
+    if (error) {
+      setMessage(error);
+      return;
+    }
+    inputEl.current.value = "";
+    setMessage("Success! 🎉 You are now subscribed to the newsletter.");
+  };
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={subscribe} className={styles.formContainer}>
+        <input
+          className={styles.input}
+          id="email-input"
+          name="email"
+          placeholder="you@stackii.com"
+          ref={inputEl}
+          required
+          type="email"
+        />
+        <div className={styles.message}>
+          {message
+            ? message
+            : `I'll only send emails when new content is posted. No spam.`}
+        </div>
+        <button type="submit" className={styles.button}>
+          Subscribe
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Newsletter;
