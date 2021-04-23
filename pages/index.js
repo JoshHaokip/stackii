a0ce7ec84705ea796cf77dc964a2b795646a91d6
JoshHaokip <joshhaokip@gmail.com>
2021-04-23 23:02:52 +0100

    Newsletter Page: Built and tested the mailchimp integration

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -30,7 +31,7 @@ export default function Home() {
               </a>
 
               <button className={`${styles.btnCard} ${styles.btnSecondary}`}>
-                get updates
+                <Link href="/newsletter">get updates</Link>
               </button>
             </div>
           </div>
--- pages/newsletter.js
+++ pages/newsletter.js
@@ -1,54 +1,103 @@
-import React, { useRef, useState } from "react";
+import React from "react";
+import Link from "next/link";
 import styles from "../styles/Newsletter.module.css";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
 
-const Newsletter = () => {
-  const inputEl = useRef(null);
-  const [message, setMessage] = useState("");
-
-  const subscribe = async (e) => {
-    e.preventDefault();
-
-    const res = await fetch("/api/subscribe", {
-      body: JSON.stringify({
-        email: inputEl.current.value,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "POST",
+const CustomForm = ({ status, message, onValidated }) => {
+  let email, name;
+  const submit = () =>
+    email &&
+    name &&
+    email.value.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email.value,
+      FNAME: name.value,
     });
 
-    const { error } = await res.json();
+  return (
+    <div className={styles.formContainer}>
+      <div className={styles.formInputContainer}>
+        <input
+          className={styles.formInput}
+          ref={(node) => (name = node)}
+          type="text"
+          placeholder="Tim Cook"
+        />
+        <br />
+        <input
+          className={styles.formInput}
+          ref={(node) => (email = node)}
+          type="email"
+          placeholder="tim@apple.com"
+        />
+        {status === "sending" && (
+          <div style={{ color: "#fe7624" }}>sending...</div>
+        )}
+        {status === "error" && (
+          <div
+            style={{ color: "red" }}
+            dangerouslySetInnerHTML={{ __html: message }}
+          />
+        )}
+        {status === "success" && (
+          <div
+            style={{ color: "#5d20d1" }}
+            dangerouslySetInnerHTML={{ __html: message }}
+          />
+        )}
+        <br />
+        <div className={styles.buttonContainer}>
+          <button className={styles.submit} onClick={submit}>
+            Subscribe
+          </button>
+          <button className={styles.back}>
+            <Link href="/">Back</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    if (error) {
-      setMessage(error);
-      return;
-    }
-    inputEl.current.value = "";
-    setMessage("Success! 🎉 You are now subscribed to the newsletter.");
-  };
+const Newsletter = () => {
+  const url =
+    "https://dev.us1.list-manage.com/subscribe/post?u=ca070fb7a10b409a14221388e&amp;id=4f56eb8533";
 
   return (
     <div className={styles.container}>
-      <form onSubmit={subscribe} className={styles.formContainer}>
-        <input
-          className={styles.input}
-          id="email-input"
-          name="email"
-          placeholder="you@stackii.com"
-          ref={inputEl}
-          required
-          type="email"
+      <style global jsx>{`
+        html,
+        body,
+        body > div:first-child,
+        div#__next,
+        div#__next > div {
+          height: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
+      <div className={styles.newsletterContainer}>
+        <h1 className={styles.title}>📩 stackii newsletter</h1>
+        <h3 className={styles.subHeading}>
+          Bi-weekly newsletter with all the best tools and resouces to help you
+          build products
+        </h3>
+        <p className={styles.bonus}>
+          Sign up before 31st May to find out how I built stackii for less than
+          $10
+        </p>
+        <MailchimpSubscribe
+          url={url}
+          render={({ subscribe, status, message }) => (
+            <CustomForm
+              status={status}
+              message={message}
+              onValidated={(formData) => subscribe(formData)}
+            />
+          )}
         />
-        <div className={styles.message}>
-          {message
-            ? message
-            : `I'll only send emails when new content is posted. No spam.`}
-        </div>
-        <button type="submit" className={styles.button}>
-          Subscribe
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
